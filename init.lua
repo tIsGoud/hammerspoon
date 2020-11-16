3b76eeac4a9e171bfde148ba2307511b7070e4fb
tIsGoud <a.w.alberts@tisgoud.nl>
2020-11-16 01:02:17 +0100

    Hyperkey working

--- init.lua
+++ init.lua
@@ -1,81 +1,132 @@
-------------------------------------------------------------
+-----------------------------------------------------------------------------
 -- Init.lua
-------------------------------------------------------------
+-----------------------------------------------------------------------------
 hs.logger.defaultLogLevel="info"
+hs.window.animationDuration = 0
 
---require "hammer"
---require "windowmanagement"
-require "wifinotifier"
+-- "hyper" is configured as "tab" in Karabiner-Elements
+hyper = {"ctrl","'alt","'cmd","shift"}
+ctrl_opt = {"ctrl","option"}
+ctrl_cmd = {"ctrl","cmd"}
+ctrl_shift = {"ctrl", "shift"}
 
--- Capture the hostname, so we can make this config behave differently across my Macs
+-- Get the hostname
 hostname = hs.host.localizedName()
 
-------------------------------------------------------------
--- Reload config on write
-------------------------------------------------------------
-local function reload_config()
-  hs.reload()
-end
-
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
-hs.alert.show("Hammerspoon config (re)loaded")
-
------------------------------------------------
--- Bind the hammer key
------------------------------------------------
-
--- The clean modal keybindings are made possible due to a comment from Steve Kehlet (github/skehlet),
--- he mentioned that there is no need to assign a key to the modal key.
--- With that a much cleaner piece of code.
--- Important here is the Karabiner-elements configuration where the "to_if_alone"-section
--- is used to restore the original key function.
-
--- In karabiner-elements set 'tab' to f18/escape
-
-hammer = hs.hotkey.modal.new()
-f18 = hs.hotkey.bind({}, 'f18', function () hammer:enter() end, function () hammer:exit() end)
-
-------------------------------------------------------------
+-----------------------------------------------------------------------------
 -- Load SpoonInstall, so we can easily load our other Spoons
-------------------------------------------------------------
+-----------------------------------------------------------------------------
 hs.loadSpoon("SpoonInstall")
 spoon.SpoonInstall.use_syncinstall = true
 Install=spoon.SpoonInstall
 
-------------------------------------------------------------
--- Draw a circle around the mouse cursor
-------------------------------------------------------------
-Install:andUse("MouseCircle",
-                {
-                    disable = false,
-                    config = {
-                        color = hs.drawing.color.x11.darkred
-                },
-                hotkeys = {
-                    show = { {hammer}, "m" }
-                }
-            }
-)
-------------------------------------------------------------
--- Direct URLs automatically based on patterns
--- See https://github.com/zzamboni/dot-hammerspoon/blob/master/init.org
-------------------------------------------------------------
-Install:andUse("URLDispatcher",
+-----------------------------------------------------------------------------
+-- ReloadConfiguration
+-----------------------------------------------------------------------------
+-- Install:andUse("ReloadConfiguration",
+--   {
+--     watch_paths = hs.configdir,
+--     hotkeys = {
+--       reloadConfiguration =  { hyper, "r" }
+--     }
+--   }
+-- )
+
+hs.hotkey.bind(hyper, "r", function()
+  hs.reload()
+end)
+
+-----------------------------------------------------------------------------
+-- MiroWindowsManager, hotkey plus arrows position and size the wondows
+-----------------------------------------------------------------------------
+Install:andUse("MiroWindowsManager",
   {
     config = {
-      url_patterns = {
-          {"https?://%w+.zoom.us", "us.zoom.xos", ""}
-      },
-      url_redir_decoders = {
-        -- Send MS Teams URLs directly to the app
-        { "MS Teams URLs",
-        "(https://teams.microsoft.com.*)", "msteams:%1", true },
-        -- Preview incorrectly encodes the anchor
-        -- character in URLs as %23, we fix it
-        { "Fix broken Preview anchor URLs", "%%23", "#", false, "Preview" },
-      },
-      default_handler = "com.apple.Safari"
+      sizes = {2, 3, 3/2}
     },
-    start = true
+    hotkeys = {
+      up =         { ctrl_opt, "up" },
+      right =      { ctrl_opt, "right" },
+      down =       { ctrl_opt, "down" },
+      left =       { ctrl_opt, "left" },
+      fullscreen = { ctrl_opt, "return" }
+    }
   }
 )
+
+-----------------------------------------------------------------------------
+-- WindowScreenLeftAndRight, move window to left or right screen
+-- default hotkeys ("ctrl", "alt", "cmd") and left- or right arrow
+-----------------------------------------------------------------------------
+Install:andUse("WindowScreenLeftAndRight",
+  {
+    config = {
+      animationDuration = 0
+    },
+    hotkeys = {
+      screen_left = {  ctrl_shift, "left"},
+      screen_right = { ctrl_shift, "right"}
+    }
+  }
+)
+
+-----------------------------------------------------------------------------
+-- MouseCircle, draw a darkred circle around the mouse cursor
+-----------------------------------------------------------------------------
+Install:andUse("MouseCircle",
+  {
+    config = {
+      color = hs.drawing.color.x11.darkred
+    },
+    hotkeys = {
+      show = { hyper, "m" }
+    }
+  }
+)
+
+-----------------------------------------------------------------------------
+-- PopupTranslateSelection, show a popup window with the translation of the
+-- currently selected (or other) text
+-----------------------------------------------------------------------------
+local wm=hs.webview.windowMasks
+Install:andUse("PopupTranslateSelection",
+  {
+    -- config = {
+    --   popup_style = wm.utility|wm.HUD|wm.titled|
+    --     wm.closable|wm.resizable,
+    -- },
+    hotkeys = {
+      translate_nl_en = { hyper, "e" },
+      translate_en_nl = { hyper, "d" }
+    }
+  }
+)
+
+-----------------------------------------------
+-- WiFinotifier.lua
+-----------------------------------------------
+
+-- Current functionality is just a notification with the network name
+-- Would be nice to turn on a VPN on an untrusted WiFi network
+
+wifiWatcher = nil
+homeSSID = "tisgoud"
+workSSID = ""
+lastSSID = hs.wifi.currentNetwork()
+
+function ssidChangedCallback()
+    newSSID = hs.wifi.currentNetwork()
+
+    if newSSID == homeSSID and lastSSID ~= homeSSID then
+        -- We just joined our home WiFi network
+        hs.notify.new({title="WiFI", informativeText=string.format("Connected to %q", homeSSID)}):send()
+    elseif newSSID ~= homeSSID and lastSSID == homeSSID then
+        -- We just departed our home WiFi network
+        hs.alert.show(string.format("WiFi connected to %q", newSSID), 3)
+    end
+
+    lastSSID = newSSID
+end
+
+wifiWatcher = hs.wifi.watcher.new(ssidChangedCallback)
+wifiWatcher:start()
